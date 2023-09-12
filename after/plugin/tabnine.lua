14eb385f034edf5462d9afa51760c6e2ad8e1edf
Andrew Brunker <andrew.brunker@lymlive.com.au>
2023-09-12 12:52:17 +1000

    Update tabnine log file path

--- after/plugin/tabnine.lua
+++ after/plugin/tabnine.lua
@@ -1,4 +1,3 @@
-
 require("tabnine").setup({
     disable_auto_comment=true,
     accept_keymap="<Tab>",
@@ -6,7 +5,7 @@ require("tabnine").setup({
     debounce_ms = 800,
     suggestion_color = {gui = "#808080", cterm = 244},
     exclude_filetypes = {"TelescopePrompt"},
-    log_file_path = nil, -- absolute path to Tabnine log file
+    log_file_path = "~/AppData/Local/nvim-data/tabnine.log", -- absolute path to Tabnine log file
 })
 
 require("tabnine.status").status()
