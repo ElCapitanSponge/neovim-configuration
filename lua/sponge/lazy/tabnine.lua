fd4058f485f75dd4c141d5f9b61667b4dfa4fa16
Andrew Brunker <andrew@brunker.net.au>
2024-01-06 10:01:43 +1100

    FEAT: Configure eslint and tabnine for lazy

--- lua/sponge/lazy/tabnine.lua
+++ lua/sponge/lazy/tabnine.lua
@@ -1,3 +1,25 @@
+local function tabnine_build_path()
+    if vim.loop.os_uname().sysname == "Windows_NT" then
+        return "pwsh.exe -file .\\dl_binaries.ps1"
+    else
+        return "./dl_binaries.sh"
+    end
+end
+
 return {
-    -- TODO: Imeplement tabnine from packer
+    {
+        "codota/tabnine-nvim",
+        build = tabnine_build_path(),
+        config = function()
+            require("tabnine").setup({
+                disable_auto_comment = true,
+                accept_keymap = "<M-]>",
+                dismiss_keymap = "<C-]>",
+                debounce_ms = 800,
+                suggestion_color = { gui = "#808080", cterm = 244 },
+                exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+                log_file_path = nil,
+            })
+        end
+    }
 }
