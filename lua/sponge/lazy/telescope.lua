d06e7eea3c34e33d87d0005a6cdcabfb6b35d768
Andrew Brunker <andrew@brunker.net.au>
2024-01-05 11:46:09 +1100

    FEAT: Implement telescope projects

--- lua/sponge/lazy/telescope.lua
+++ lua/sponge/lazy/telescope.lua
@@ -3,10 +3,28 @@ return {
         "nvim-telescope/telescope.nvim",
         version = "0.1.5",
         dependencies = {
-            "nvim-lua/plenary.nvim"
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-project.nvim"
         },
         config = function()
-            require("telescope").setup({})
+            require("telescope").setup({
+                defaults = {
+                    path_display = {
+                        "smart"
+                    }
+                },
+                extensions = {
+                    project = {
+                        base_dirs = {
+                            '~/AppData/Local/',
+                            '~/Documents/sources/',
+                        }
+                    }
+                }
+            })
+
+            require('telescope').load_extension('project')
+
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "[f]ind [f]iles search" })
             vim.keymap.set('n', '<leader>lg', builtin.live_grep, { desc = "[l]ive [g]rep" })
